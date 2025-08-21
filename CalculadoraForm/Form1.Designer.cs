ca1eefad16c5853f115e5cad46fc3ab9d86c2857
CBKupper <camillavitoriakupper@gmail.com>
2025-08-20 22:05:01 -0300

    Funcionamento inicial
    Foi criado o código, utilizando objeto sender, para implementar o funcionamento básico da calculadora: interação dos botões com a tela

--- CalculadoraForm/Form1.Designer.cs
+++ CalculadoraForm/Form1.Designer.cs
@@ -53,130 +53,140 @@
             this.btn7.FlatAppearance.BorderSize = 2;
             this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn7.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn7.ForeColor = System.Drawing.Color.White;
+            this.btn7.ForeColor = System.Drawing.Color.Black;
             this.btn7.Location = new System.Drawing.Point(15, 186);
             this.btn7.Name = "btn7";
             this.btn7.Size = new System.Drawing.Size(84, 56);
             this.btn7.TabIndex = 0;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn8
             // 
             this.btn8.FlatAppearance.BorderSize = 2;
             this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn8.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn8.ForeColor = System.Drawing.Color.White;
+            this.btn8.ForeColor = System.Drawing.Color.Black;
             this.btn8.Location = new System.Drawing.Point(105, 186);
             this.btn8.Name = "btn8";
             this.btn8.Size = new System.Drawing.Size(84, 56);
             this.btn8.TabIndex = 1;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn9
             // 
             this.btn9.FlatAppearance.BorderSize = 2;
             this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn9.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn9.ForeColor = System.Drawing.Color.White;
+            this.btn9.ForeColor = System.Drawing.Color.Black;
             this.btn9.Location = new System.Drawing.Point(195, 186);
             this.btn9.Name = "btn9";
             this.btn9.Size = new System.Drawing.Size(84, 56);
             this.btn9.TabIndex = 2;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn6
             // 
             this.btn6.FlatAppearance.BorderSize = 2;
             this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn6.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn6.ForeColor = System.Drawing.Color.White;
+            this.btn6.ForeColor = System.Drawing.Color.Black;
             this.btn6.Location = new System.Drawing.Point(195, 248);
             this.btn6.Name = "btn6";
             this.btn6.Size = new System.Drawing.Size(84, 56);
             this.btn6.TabIndex = 5;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn5
             // 
             this.btn5.FlatAppearance.BorderSize = 2;
             this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn5.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn5.ForeColor = System.Drawing.Color.White;
+            this.btn5.ForeColor = System.Drawing.Color.Black;
             this.btn5.Location = new System.Drawing.Point(105, 248);
             this.btn5.Name = "btn5";
             this.btn5.Size = new System.Drawing.Size(84, 56);
             this.btn5.TabIndex = 4;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn4
             // 
             this.btn4.FlatAppearance.BorderSize = 2;
             this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn4.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn4.ForeColor = System.Drawing.Color.White;
+            this.btn4.ForeColor = System.Drawing.Color.Black;
             this.btn4.Location = new System.Drawing.Point(15, 248);
             this.btn4.Name = "btn4";
             this.btn4.Size = new System.Drawing.Size(84, 56);
             this.btn4.TabIndex = 3;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn3
             // 
             this.btn3.FlatAppearance.BorderSize = 2;
             this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn3.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn3.ForeColor = System.Drawing.Color.White;
+            this.btn3.ForeColor = System.Drawing.Color.Black;
             this.btn3.Location = new System.Drawing.Point(195, 310);
             this.btn3.Name = "btn3";
             this.btn3.Size = new System.Drawing.Size(84, 56);
             this.btn3.TabIndex = 8;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn2
             // 
             this.btn2.FlatAppearance.BorderSize = 2;
             this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn2.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn2.ForeColor = System.Drawing.Color.White;
+            this.btn2.ForeColor = System.Drawing.Color.Black;
             this.btn2.Location = new System.Drawing.Point(105, 310);
             this.btn2.Name = "btn2";
             this.btn2.Size = new System.Drawing.Size(84, 56);
             this.btn2.TabIndex = 7;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn1
             // 
             this.btn1.FlatAppearance.BorderSize = 2;
             this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn1.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn1.ForeColor = System.Drawing.Color.White;
+            this.btn1.ForeColor = System.Drawing.Color.Black;
             this.btn1.Location = new System.Drawing.Point(15, 310);
             this.btn1.Name = "btn1";
             this.btn1.Size = new System.Drawing.Size(84, 56);
             this.btn1.TabIndex = 6;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.numero_Click);
             // 
             // btn0
             // 
             this.btn0.FlatAppearance.BorderSize = 2;
             this.btn0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn0.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn0.ForeColor = System.Drawing.Color.White;
+            this.btn0.ForeColor = System.Drawing.Color.Black;
             this.btn0.Location = new System.Drawing.Point(15, 374);
             this.btn0.Name = "btn0";
             this.btn0.Size = new System.Drawing.Size(174, 84);
             this.btn0.TabIndex = 9;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.numero_Click);
             // 
             // btnIgual
             // 
@@ -185,65 +195,70 @@
             this.btnIgual.FlatAppearance.BorderSize = 2;
             this.btnIgual.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIgual.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIgual.ForeColor = System.Drawing.Color.White;
+            this.btnIgual.ForeColor = System.Drawing.Color.Black;
             this.btnIgual.Location = new System.Drawing.Point(285, 374);
             this.btnIgual.Name = "btnIgual";
             this.btnIgual.Size = new System.Drawing.Size(79, 84);
             this.btnIgual.TabIndex = 10;
             this.btnIgual.Text = "=";
             this.btnIgual.UseVisualStyleBackColor = false;
+            this.btnIgual.Click += new System.EventHandler(this.btnIgual_Click);
             // 
             // btnDividir
             // 
             this.btnDividir.FlatAppearance.BorderSize = 2;
             this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDividir.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDividir.ForeColor = System.Drawing.Color.White;
+            this.btnDividir.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.ForeColor = System.Drawing.Color.Black;
             this.btnDividir.Location = new System.Drawing.Point(285, 186);
             this.btnDividir.Name = "btnDividir";
-            this.btnDividir.Size = new System.Drawing.Size(79, 45);
+            this.btnDividir.Size = new System.Drawing.Size(79, 44);
             this.btnDividir.TabIndex = 13;
             this.btnDividir.Text = "/";
             this.btnDividir.UseVisualStyleBackColor = true;
+            this.btnDividir.Click += new System.EventHandler(this.operador_Click);
             // 
             // btnAdicionar
             // 
             this.btnAdicionar.FlatAppearance.BorderSize = 2;
             this.btnAdicionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdicionar.ForeColor = System.Drawing.Color.White;
+            this.btnAdicionar.ForeColor = System.Drawing.Color.Black;
             this.btnAdicionar.Location = new System.Drawing.Point(285, 328);
             this.btnAdicionar.Name = "btnAdicionar";
             this.btnAdicionar.Size = new System.Drawing.Size(79, 38);
             this.btnAdicionar.TabIndex = 14;
             this.btnAdicionar.Text = "+";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.operador_Click);
             // 
             // btnSubtrair
             // 
             this.btnSubtrair.FlatAppearance.BorderSize = 2;
             this.btnSubtrair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubtrair.ForeColor = System.Drawing.Color.White;
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Tai Le", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.ForeColor = System.Drawing.Color.Black;
             this.btnSubtrair.Location = new System.Drawing.Point(285, 281);
             this.btnSubtrair.Name = "btnSubtrair";
             this.btnSubtrair.Size = new System.Drawing.Size(79, 41);
             this.btnSubtrair.TabIndex = 15;
             this.btnSubtrair.Text = "-";
             this.btnSubtrair.UseVisualStyleBackColor = true;
+            this.btnSubtrair.Click += new System.EventHandler(this.operador_Click);
             // 
             // btnMultiplicar
             // 
             this.btnMultiplicar.FlatAppearance.BorderSize = 2;
             this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMultiplicar.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMultiplicar.ForeColor = System.Drawing.Color.White;
-            this.btnMultiplicar.Location = new System.Drawing.Point(285, 237);
+            this.btnMultiplicar.ForeColor = System.Drawing.Color.Black;
+            this.btnMultiplicar.Location = new System.Drawing.Point(285, 236);
             this.btnMultiplicar.Name = "btnMultiplicar";
-            this.btnMultiplicar.Size = new System.Drawing.Size(79, 38);
+            this.btnMultiplicar.Size = new System.Drawing.Size(79, 39);
             this.btnMultiplicar.TabIndex = 16;
             this.btnMultiplicar.Text = "*";
             this.btnMultiplicar.UseVisualStyleBackColor = true;
+            this.btnMultiplicar.Click += new System.EventHandler(this.operador_Click);
             // 
             // btnC
             // 
@@ -251,7 +266,7 @@
             this.btnC.FlatAppearance.BorderSize = 2;
             this.btnC.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnC.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnC.ForeColor = System.Drawing.Color.White;
+            this.btnC.ForeColor = System.Drawing.Color.Black;
             this.btnC.Location = new System.Drawing.Point(195, 374);
             this.btnC.Name = "btnC";
             this.btnC.Size = new System.Drawing.Size(84, 84);
@@ -263,12 +278,12 @@
             // 
             this.txbTela.BackColor = System.Drawing.Color.CadetBlue;
             this.txbTela.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txbTela.Font = new System.Drawing.Font("Microsoft Tai Le", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbTela.ForeColor = System.Drawing.Color.White;
-            this.txbTela.Location = new System.Drawing.Point(49, 47);
+            this.txbTela.Font = new System.Drawing.Font("Microsoft Tai Le", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTela.ForeColor = System.Drawing.Color.Black;
+            this.txbTela.Location = new System.Drawing.Point(46, 54);
             this.txbTela.Name = "txbTela";
             this.txbTela.ReadOnly = true;
-            this.txbTela.Size = new System.Drawing.Size(236, 45);
+            this.txbTela.Size = new System.Drawing.Size(285, 82);
             this.txbTela.TabIndex = 18;
             this.txbTela.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -277,7 +292,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.CadetBlue;
-            this.ClientSize = new System.Drawing.Size(456, 470);
+            this.ClientSize = new System.Drawing.Size(379, 470);
             this.Controls.Add(this.txbTela);
             this.Controls.Add(this.btnC);
             this.Controls.Add(this.btnMultiplicar);
@@ -295,6 +310,7 @@
             this.Controls.Add(this.btn9);
             this.Controls.Add(this.btn8);
             this.Controls.Add(this.btn7);
+            this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
--- CalculadoraForm/Form1.cs
+++ CalculadoraForm/Form1.cs
@@ -12,9 +12,56 @@ namespace CalculadoraForm
 {
     public partial class Form1 : Form
     {
+        // variáveis globais:
+        bool operadorClicado = true;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            // Implementar depois...
+
+
+        }
+
+        private void numero_Click(object sender, EventArgs e)
+        { 
+           // Obter o botão que esta chamando esse evento:
+           Button botaoClicar = (Button)sender;
+
+            // Adicionar o text do botão clicado no textnox:
+            txbTela.Text += botaoClicar.Text;
+
+            // "abaixar a bandeirinha"
+            operadorClicado = false;
+
+        }
+
+        private void operador_Click(object sender, EventArgs e)
+        {
+            // verificar se o operador ainda não foi clicacdo:
+            if (operadorClicado == false)
+            {
+                // Obter o botão que esta chamando esse evento:
+                Button botaoClicar = (Button)sender;
+
+                // Adicionar o text do botão clicado no textnox:
+                txbTela.Text += botaoClicar.Text;
+
+                // mudar o operadorClicado para true:
+                operadorClicado = true;
+
+
+            }
+
+
+           
+
+        }
+
+
     }
 }
